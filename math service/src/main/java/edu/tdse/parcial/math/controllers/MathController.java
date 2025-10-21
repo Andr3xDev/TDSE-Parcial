2333dc8fbf60ac29095f3e2c6da0803c048d17eb
Andr3xDev <Andr3xDev@gmail.com>
2025-10-21 10:17:09 -0500

    feat: add sequence calculator full

--- math service/src/main/java/edu/tdse/parcial/math/controllers/MathController.java
+++ math service/src/main/java/edu/tdse/parcial/math/controllers/MathController.java
@@ -1,15 +1,29 @@
 package edu.tdse.parcial.math.controllers;
 
+import edu.tdse.parcial.math.serivice.MathService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class MathController {
 
-    @GetMapping()
-    public Greeting greeting(@RequestParam(value = "value") String value) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    private final MathService mathService;
+
+    public MathController(MathService mathService){
+        this.mathService = mathService;
+    }
+
+    /**
+     * Endpoint to calculate Collatz Sequence
+     *
+     * @param value value to start sequence
+     * @return sequence full
+     */
+    @GetMapping("sequence")
+    public List<Integer> secuence(@RequestParam(value = "value") Integer value) {
+        return mathService.collatzSequence(value);
     }
 
 }
--- math service/src/main/java/edu/tdse/parcial/math/serivice/MathService.java
+++ math service/src/main/java/edu/tdse/parcial/math/serivice/MathService.java
@@ -8,18 +8,28 @@ import java.util.List;
 @Service
 public class MathService {
 
+    /**
+     * Method to calculate Collatz Sequence
+     *
+     * @param value value to start the sequence
+     * @return sequence list of the values of the sequence
+     */
     public List<Integer> collatzSequence(int value){
+        int newValue = value;
         List<Integer> sequence = new ArrayList<>();
 
-        if (value == 1) {
-            return sequence;
-        }
+        sequence.add(value);
 
-        if (value % 2 == 0){
-            value =
-        } else {
-            
+        while (newValue != 1){
+            if(newValue % 2 == 0){
+                newValue = newValue/2;
+                sequence.add(newValue);
+            } else {
+                newValue = (3*newValue + 1);
+                sequence.add(newValue);
+            }
         }
+        return sequence;
     }
 
 }
