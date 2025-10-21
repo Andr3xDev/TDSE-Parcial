d4f11077857eacc84654f0df4794277b80b86e14
Andr3xDev <Andr3xDev@gmail.com>
2025-10-21 10:30:15 -0500

    feat: add basic index & controllers

---
+++ proxy/src/main/java/edu/tdse/parcial/proxy/controller/ProxyController.java
@@ -0,0 +1,9 @@
+package edu.tdse.parcial.proxy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProxyController {
+
+}
